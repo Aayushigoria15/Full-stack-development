fbca382c757e2eae5a85b27ad79c6859c2865e8e
Aayushigoria15 <157952434+Aayushigoria@users.noreply.github.com>
2025-03-09 16:56:41 +0530

    Assignments

---
+++ javascript/ex.js
@@ -0,0 +1,10 @@
+console.log("hello this external js");
+document.write("hello dom method");
+
+// bom :- browser object model
+// logic function
+
+alert("pls filed the form");
+confirm("you are confirming the deatils?");
+prompt(" 5 + 5 => ?");
+// print();
